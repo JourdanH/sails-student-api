b599bc5293e4ebea88ccf7230d6a0ec418538022
sparrowlark <30511105+sparrowlark@users.noreply.github.com>
2017-09-18 07:47:51 -0700

    finished API

---
+++ api/controllers/ClassController.js
@@ -0,0 +1,10 @@
+/**
+ * classController
+ *
+ * @description :: Server-side logic for managing classes
+ * @help        :: See http://sailsjs.org/#!/documentation/concepts/Controllers
+ */
+
+module.exports = {
+
+};
